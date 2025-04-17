05d2033cf7530017c634077f4d3da2685ab86bff
Gospel Chinyereugo <gospelokpara@gmail.com>
2025-04-17 02:15:40 +0100

    feat: worked on api auth via supabase access token

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,19 +1,9 @@
-import { getCookie, handleLogoutRedirect } from "@/lib/utils";
+import { handleLogoutRedirect } from "@/lib/utils";
 import axios from "axios";
 
 const client = axios.create({
   // baseURL: import.meta.env.PUBLIC_API_URL,
   baseURL: import.meta.env.VITE_API_URL,
-});
-
-client.interceptors.request.use(function (config) {
-  let token = getCookie("_tk");
-  if (token) {
-    token = "Bearer " + token;
-    config.headers.authorization = token;
-  }
-
-  return config;
 });
 
 client.interceptors.response.use(
